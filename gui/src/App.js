2c93fd294036de8b97e5585f725f50b5a166ea90
jramos-tech <jramos@student.neumont.edu>
2025-01-09 13:18:02 -0700

    Setting up ReactJS project for Javascript and HTML

---
+++ gui/src/App.js
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div className={"Iamai"}>
+      <div className="rectangle-5"></div>
+      <div className="rectangle-24"></div>
+      <div className="rectangle-2"></div>
+      <img className="paper-clip-light" src="paper-clip-light0.svg" />
+      <div className="what-s-on-your-mind">What’s on your mind? </div>
+      <div className="rectangle-3"></div>
+      <div className="last-prompt">Last Prompt </div>
+      <div className="rectangle-4"></div>
+      <div className="ai-response">AI Response </div>
+      <div className="rectangle-7"></div>
+      <img className="menu" src="menu0.svg" />
+      <div className="iamai">Iamai </div>
+      <img className="image-1" src="image-10.png" />
+    </div>
+  );
+};
+
+export default App;
---
+++ gui/src/SideBar.js
@@ -0,0 +1,18 @@
+function SideBar() {
+    return (
+        <div className={"pop-up-1 " + className}>
+          <div className="rectangle-8"></div>
+          <Menu className="menu-instance"></Menu>
+          <img className="rectangle-9" src="rectangle-90.svg" />
+          <div className="settings">Settings </div>
+          <img className="setting-line-light" src="setting-line-light0.svg" />
+          <img className="add-square-light" src="add-square-light0.svg" />
+          <div className="ellipse-6"></div>
+          <div className="john-doe">John Doe </div>
+          <div className="line-2"></div>
+          <div className="line-1"></div>
+        </div>
+      );
+}
+
+export default SideBar;
